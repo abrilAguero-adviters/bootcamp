a71cc75fc23d849db1d893e1481e6ac1d51b2d11
Kevinnahuelf <80533588+zycness@users.noreply.github.com>
2024-01-22 20:37:05 -0300

    cambios en ejercicio de array

--- JS/04-arrays/index.js
+++ JS/04-arrays/index.js
@@ -1,3 +1,5 @@
-//Tengo una lista de productos para comprar, pero tengo que cambiar el orden
+// 1 - Tengo una lista de productos para comprar, pero tengo que cambiar el orden: tiene que ser de menos caracteres a mayores
+// 2 - También tengo que agregar 2 productos, uno al inicio y otro al final.
+// 3 - Pero me arrepentí, quiero sacar el último.
 
 let listaCompras = ["Manzanas", "Plátanos", "Leche", "Pan"];
