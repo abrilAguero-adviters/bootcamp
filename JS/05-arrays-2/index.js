feb207a2b7f81af16b1038f9714dc9a2860b1915
Zycness <80533588+zycness@users.noreply.github.com>
2024-01-23 01:33:59 -0300

    ejercicios 05 06 07

---
+++ JS/05-arrays-2/index.js
@@ -0,0 +1,41 @@
+// Crear un arreglo inicial de números
+// Crear un arreglo de objetos que representan libros
+const biblioteca = [
+  {
+    id: 1,
+    titulo: 'El señor de los anillos',
+    autor: 'J.R.R. Tolkien',
+    genero: 'Fantasia',
+  },
+  {
+    id: 2,
+    titulo: 'Cien años de soledad',
+    autor: 'Gabriel García Márquez',
+    genero: 'Realismo mágico',
+  },
+  { id: 3, titulo: '1984', autor: 'George Orwell', genero: 'Ciencia ficción' },
+  {
+    id: 4,
+    titulo: 'Matar un ruiseñor',
+    autor: 'Harper Lee',
+    genero: 'Ficción',
+  },
+  { id: 1, titulo: 'Harry Potter', autor: 'J. K. Rowling', genero: 'Fantasia' },
+];
+
+// Utilizar 'map' para obtener un arreglo con los títulos de los libros
+// console.log('Títulos de libros:', titulosLibros);
+
+// Utilizar 'filter' para obtener los libros del género 'Fantasía'
+// console.log('Libros de fantasía:', librosFantasia);
+
+// Utilizar 'find' para encontrar el libro con título '1984'
+// console.log('Libro con título "1984":', libro1984);
+
+// Utilizar 'findIndex' para encontrar el índice del libro con autor 'Harper Lee'
+// console.log('Índice del libro de Harper Lee:', indiceHarperLee);
+
+// Utilizar el operador de propagación para agregar un nuevo libro a la biblioteca
+// const nuevoLibro = { id: 5, titulo: 'Don Quijote de la Mancha', autor: 'Miguel de Cervantes', genero: 'Clásico' };
+
+// console.log('Biblioteca después de agregar un nuevo libro:', bibliotecaActualizada);
