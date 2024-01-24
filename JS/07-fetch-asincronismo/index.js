a5e58b6ad3a6204e16513a71aaa15c05a89e8cf1
abrilAguero-adviters <abril.aguero@adviters.com>
2024-01-24 15:13:17 -0300

    ejercicios en clase

--- JS/07-fetch-asincronismo/index.js
+++ JS/07-fetch-asincronismo/index.js
@@ -1,5 +1,5 @@
-const obtenerDatosDeAPI = (url) => {
-  return fetch(url).then((response) => {
+const obtenerDatosDeAPI = async (url) => {
+  return await fetch(url).then((response) => {
     if (!response.ok) {
       throw new Error(
         `Error al obtener los datos. Código de estado: ${response.status}`
@@ -9,21 +9,39 @@ const obtenerDatosDeAPI = (url) => {
   });
 };
 
-const main = () => {
-  // const url = 'https://jsonplaceholder.typicode.com/todos/1';
-  // const url = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0';
+// async function funcion () {
+// }
+
+// const funcion1 = async () => {
+// }
+
+const main = async () => {
+  // const url = "https://jsonplaceholder.typicode.com/todos/1";
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0";
 
   console.log("Iniciando la solicitud de datos...");
 
-  obtenerDatosDeAPI(url)
+  const pokemons = await obtenerDatosDeAPI(url)
     .then((data) => {
       console.log("Datos obtenidos con éxito:", JSON.stringify(data, null, 2));
+      return data;
     })
     .catch((error) => {
       console.error("Error al obtener datos:", error);
     });
 
   console.log("Solicitud de datos en progreso...");
+  console.log(pokemons.results);
+
+  console.log(
+    pokemons.results.map(({ name }, index) => ({
+      [`Pokemon-${index}`]: `Hola soy el pokemon ${name}`,
+    }))
+  );
 };
 
 main();
+
+// API -->  Aplication Program Interface --> conexión entre el cliente (front-end) y servidor (back-end) --> establece un conjunto de solicitudes (con el protocolo HTTP) --> establece un modelo de datos --> y una forma de comunicación entre servicios
+
+// front-end -----> back-end ---> base de datos
